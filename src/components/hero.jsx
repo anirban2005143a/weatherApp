62c8da8af5111653dd69151f08a2a7cbe571bb02
anirban2005143a <23je0104@iitism.ac.in>
2024-10-27 04:20:25 +0530

    fgth

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -43,25 +43,25 @@ const Hero = (props) => {
                     <div className='slide1 w-1/4 h-full bg-cover bg-blend-hard-light  bg-[rgba(0,0,0,0.6)] text-white flex justify-center items-center ' style={{ backgroundImage: `url(${bgImg1})` }}>
                         <div className='content text-center backdrop-blur-md bg-[rgba(175,64,64,0.1)] md:w-7/12 w-10/12 md:px-6 sm:px-3 px-2 md:py-8 sm:py-6 py-4 border-[1px] border-white rounded-xl'>
                             <h1 className='text-5xl font-bold my-8'>Forecast</h1>
-                            <p className='text-base font-normal my-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
+                            <p className='text-base font-normal my-4 md:px-6 px-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
                         </div>
                     </div>
                     <div className='slide1 w-1/4 h-full bg-cover bg-blend-hard-light  bg-[rgba(0,0,0,0.6)] text-white flex justify-center items-center' style={{ backgroundImage: `url(${bgImg2})` }}>
                         <div className='content text-center backdrop-blur-md bg-[rgba(175,64,64,0.1)] md:w-7/12 w-10/12 md:px-6 sm:px-3 px-2 md:py-8 sm:py-6 py-4 border-[1px] border-white rounded-xl'>
                             <h1 className='text-5xl font-bold my-8'>Forecast</h1>
-                            <p className='text-base font-normal my-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
+                            <p className='text-base font-normal my-4 md:px-6 px-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
                         </div>
                     </div>
                     <div className='slide1 w-1/4 h-full bg-cover bg-blend-hard-light  bg-[rgba(0,0,0,0.6)] text-white flex justify-center items-center' style={{ backgroundImage: `url(${bgImg3})` }}>
                         <div className='content text-center backdrop-blur-md bg-[rgba(175,64,64,0.1)] md:w-7/12 w-10/12 md:px-6 sm:px-3 px-2 md:py-8 sm:py-6 py-4 border-[1px] border-white rounded-xl'>
                             <h1 className='text-5xl font-bold my-8'>Forecast</h1>
-                            <p className='text-base font-normal my-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
+                            <p className='text-base font-normal my-4 md:px-6 px-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
                         </div>
                     </div>
                     <div className='slide1 w-1/4 h-full bg-cover bg-blend-hard-light  bg-[rgba(0,0,0,0.6)] text-white flex justify-center items-center' style={{ backgroundImage: `url(${bgImg4})` }}>
                         <div className='content text-center backdrop-blur-md bg-[rgba(175,64,64,0.1)] md:w-7/12 w-10/12 md:px-6 sm:px-3 px-2 md:py-8 sm:py-6 py-4 border-[1px] border-white rounded-xl'>
                             <h1 className='text-5xl font-bold my-8'>Forecast</h1>
-                            <p className='text-base font-normal my-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
+                            <p className='text-base font-normal my-4 md:px-6 px-4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapienteLorem ipsum dolor sit, amet consectetur adipisicing elit. Id porro a, doloremque at reprehenderit est magnam iusto, natus dolorum fuga sapiente </p>
                         </div>
                     </div>
                 </div>
